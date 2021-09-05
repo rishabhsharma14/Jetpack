c42b0fa6f4dd25c1b208afed1474de4af80a5aac
risha <rishabhsharmait3044@gmail.com>
2021-09-05 22:16:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+                }
+
+            }
+        }
+
